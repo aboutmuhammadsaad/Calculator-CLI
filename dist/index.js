b8858b3d02e2ff18fbb013a7ac3eeae669787711
Muhammad Saad <129781159+aboutmuhammadsaad@users.noreply.github.com>
2023-10-02 17:10:45 +0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+inquirer
+    .prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter a 1st number",
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "Enter a 2nd number",
+    },
+    {
+        type: "list",
+        name: "operation",
+        message: "Select operation",
+        choices: ["Addition", "Subtraction", "Multipication", "Division"]
+    },
+])
+    .then((answers) => {
+    if (answers.operation == "Addition") {
+        const result = answers.num1 + answers.num2;
+        console.log("Sum: ", result);
+    }
+    else if (answers.operation == "Subtraction") {
+        const result = answers.num1 - answers.num2;
+        console.log("Subtraction: ", result);
+    }
+    else if (answers.operation == "Multipication") {
+        const result = answers.num1 * answers.num2;
+        console.log("Multipication: ", result);
+    }
+    else if (answers.operation == "Division") {
+        const result = answers.num1 / answers.num2;
+        console.log("Division: ", result);
+    }
+})
+    .catch((error) => {
+    if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+    }
+    else {
+        // Something else went wrong
+    }
+});
